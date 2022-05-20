005e0ebae9f26d691b6e546836c3b8d8ba8bca0a
Fadi Zahhar <79freelancer@gmail.com>
2022-05-20 22:11:18 +0300

    adding the files

---
+++ middleware/error-handler.js
@@ -0,0 +1,7 @@
+const {CustomAPIError} = require('../errors/custom-error')
+const errorHandlerMiddleware = (err,req,res,next) => {
+    if(err instanceof CustomAPIError) {
+        return res.stauts(err.statusCode).json({msg:err.message})
+    }
+    return res.status(500).json({msg:'Something went wrong, please try again'})
+}
